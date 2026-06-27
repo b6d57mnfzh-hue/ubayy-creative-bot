9de60d15f17c0d3c1b9f5df1a5f59e1a0dd03b62
b6d57mnfzh-hue <b6d57mnfzh@privaterelay.appleid.com>
2026-06-27 18:35:14 +0100

    Create start.py

---
+++ start.py
@@ -0,0 +1,23 @@
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes
+
+TOKEN = “YOUR_BOT_TOKEN”
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+await update.message.reply_text(
+“🎉 Welcome to Ubayy Creative Studio!\n\n”
+“We offer:\n”
+“• Logo Design\n”
+“• Flyers & Posters\n”
+“• Business Cards\n”
+“• Social Media Designs\n”
+“• Certificates\n”
+“• Branding\n\n”
+“Send us a message to get started.”
+)
+
+app = Application.builder().token(TOKEN).build()
+app.add_handler(CommandHandler(“start”, start))
+
+print(“Bot running…”)
+app.run_polling()
