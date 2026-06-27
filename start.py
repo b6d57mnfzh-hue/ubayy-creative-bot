d42162858dfb92ad441e0c57de46d06015b7dc3b
b6d57mnfzh-hue <b6d57mnfzh@privaterelay.appleid.com>
2026-06-27 18:43:26 +0100

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,8 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = “YOUR_BOT_TOKEN”
+TOKEN = “8502310716:AAFf3qltc727re0ztB2CTnLWlQd8flFii3A
+”
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 await update.message.reply_text(
